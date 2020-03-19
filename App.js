ed8bda42d7f02e7b06bbf592d788ab224d417134
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2020-03-19 09:09:17 +0200

    fix android tab view

--- App.js
+++ App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { Navigation } from './src/components/views/Navigation';
-
+import { View, StatusBar } from 'react-native';
+import Constants from 'expo-constants';
+function AppStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  );
+}
 export default class App extends Component {
   render() {
     return (
       <PaperProvider>
-        <NavigationContainer>
-          <Navigation />
-        </NavigationContainer>
+        <View style={{ flex: 1 }}>
+          <AppStatusBar backgroundColor={'#1980F5'} barStyle="light-content" />
+          <NavigationContainer>
+            <Navigation />
+          </NavigationContainer>
+        </View>
       </PaperProvider>
     );
   }
--- src/components/views/Navigation.js
+++ src/components/views/Navigation.js
@@ -2,42 +2,45 @@ import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons, FontAwesome } from '@expo/vector-icons';
-
+import { Platform } from 'react-native';
 import AddDeck from './AddDeck';
 import Decks from './Decks';
 
+const tabBarOptions = {
+  style: {
+    height: 80,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowRadius: 6,
+    shadowOpacity: 1
+  }
+};
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ color, size }) =>
+    route.name === 'Add Deck' ? (
+      <FontAwesome name="plus-square" size={size} color={color} />
+    ) : (
+      <Ionicons name="ios-list-box" color={color} size={size} />
+    )
+});
 const Tab =
   Platform.OS === 'ios'
     ? createBottomTabNavigator()
     : createMaterialTopTabNavigator();
-
 export const Navigation = () => {
   return (
     <Tab.Navigator
       initialRouteName="Decks"
       shifting={true}
       sceneAnimationEnabled={false}
+      tabBarOptions={tabBarOptions}
+      screenOptions={screenOptions}
     >
-      <Tab.Screen
-        name="Decks"
-        component={Decks}
-        options={{
-          tabBarLabel: 'Decks',
-          tabBarIcon: ({ color }) => (
-            <FontAwesome name="plus-square" color={color} size={26} />
-          )
-        }}
-      />
-      <Tab.Screen
-        name="AddDeck"
-        component={AddDeck}
-        options={{
-          tabBarLabel: 'Add Deck',
-          tabBarIcon: ({ color }) => (
-            <Ionicons name="ios-list-box" color={color} size={26} />
-          )
-        }}
-      />
+      <Tab.Screen name="Decks" component={Decks} />
+      <Tab.Screen name="Add Deck" component={AddDeck} />
     </Tab.Navigator>
   );
 };
